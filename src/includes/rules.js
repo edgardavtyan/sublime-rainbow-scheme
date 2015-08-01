20b06791c2e36f5a44761c7dd6e0b5d30622cdd7
Edgar Davtyan <edgar.davtyan@outlook.com>
2015-08-01 03:54:51 +0300

    [style] Further formatting improvements

--- src/includes/rules.js
+++ src/includes/rules.js
@@ -19,21 +19,21 @@ module.exports.load = function(colors) {
 	syntax.base_class    = colors.teal;
 
 	syntax.html_doctype = colors.orange;
-	syntax.html_tag     = colors.blue;
-	syntax.html_attr    = colors.orange;
-	syntax.html_char    = colors.yellow;
+	syntax.html_tag  = colors.blue;
+	syntax.html_attr = colors.orange;
+	syntax.html_char = colors.yellow;
 
-	syntax.css_string    = syntax.base_string;
-	syntax.css_class     = syntax.html_tag;
-	syntax.css_tag       = syntax.html_tag;
-	syntax.css_pseudo    = syntax.html_tag;
-	syntax.css_atRule    = colors.yellow;
-	syntax.css_property  = colors.purple;
+	syntax.css_string   = syntax.base_string;
+	syntax.css_class    = syntax.html_tag;
+	syntax.css_tag      = syntax.html_tag;
+	syntax.css_pseudo   = syntax.html_tag;
+	syntax.css_atRule   = colors.yellow;
+	syntax.css_property = colors.purple;
 	syntax.css_meta_property = syntax.css_property;
-	syntax.css_value     = colors.orange;
-	syntax.css_unit      = syntax.css_value;
-	syntax.css_variable  = colors.teal;
-	syntax.css_mixin     = colors.yellow_alt;
+	syntax.css_value    = colors.orange;
+	syntax.css_unit     = syntax.css_value;
+	syntax.css_variable = colors.teal;
+	syntax.css_mixin    = colors.yellow_alt;
 	syntax.css_media_constant = syntax.css_value
 
 	return syntax;
