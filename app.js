23bd74a2b6318dfe72732c12470cbee08177a0a2
Edgar Davtyan <edgar.davtyan@outlook.com>
2015-08-07 04:17:56 +0300

    [feat] Add 'twilight' theme

--- app.js
+++ app.js
@@ -3,3 +3,4 @@ var plistCompiler = require('./src/plistCompiler');
 plistCompiler('dark');
 plistCompiler('light');
 plistCompiler('seti');
+plistCompiler('twilight');
---
+++ src/themes/twilight.js
@@ -0,0 +1,17 @@
+module.exports = {
+	foreground:   '#BBBBBB',
+	background:   '#141414',
+	current_line: '#333333',
+	selection:    '#336699',
+
+	blue:   '#7587A6',
+	gray:   '#5E5A60',
+	green:  '#8F9D6A',
+	orange: '#CF6A4C',
+	purple: '#9B859D',
+	red:    '#CF6A4D',
+	teal:   '#91C9BA',
+	yellow: '#CDA869',
+
+	yellow_alt: '#E4DD96',
+}
