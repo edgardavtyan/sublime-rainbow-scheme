6b1fe911b0c060c09311abf6d86bdeba866d57fa
Edgar Davtyan <edgar.davtyan@outlook.com>
2016-02-10 23:53:23 +0200

    Further darken light theme colors

--- src/themes/light.js
+++ src/themes/light.js
@@ -1,16 +1,16 @@
 module.exports = {
 	foreground:   '#444444',
-	background:   '#EFF1F5',
+	background:   '#E6E9F0',
 	current_line: '#EEEEEE',
 	selection:    '#B2D1FF',
 
 	blue:   '#0033cc',
-	gray:   '#888888',
+	gray:   '#777777',
 	green:  '#477519',
 	orange: '#C16325',
 	purple: '#B8008A',
-	red:    '#E04644',
-	teal:   '#2AA198',
+	red:    '#C52320',
+	teal:   '#1F7A72',
 	yellow: '#997A00',
 
 	yellow_alt: '#D6AD33',
