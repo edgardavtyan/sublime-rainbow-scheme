2874bd742d0a689a741fe7796f4136ffae21941d
Edgar Davtyan <edgar.davtyan@outlook.com>
2016-05-08 14:36:32 +0300

    Change light scheme colors

--- src/themes/light.js
+++ src/themes/light.js
@@ -1,14 +1,14 @@
 module.exports = {
 	foreground:   '#444444',
-	background:   '#E6E9F0',
-	current_line: '#EEEEEE',
+	background:   '#e6e6e6',
+	current_line: '#d9d9d9',
 	selection:    '#B2D1FF',
 
 	blue:   '#0033cc',
 	gray:   '#777777',
 	green:  '#477519',
 	orange: '#C16325',
-	purple: '#B8008A',
+	purple: '#764FC4',
 	red:    '#C52320',
 	teal:   '#1F7A72',
 	yellow: '#997A00',
